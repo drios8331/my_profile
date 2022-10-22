b0fda598bc197f82bd75071a7c16d8f8bb838bde
drios8331 <david.visitrack@gmail.com>
2022-10-22 11:57:10 -0500

    Imagenes ok1

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,18 +1,19 @@
 const imageSkill = require.context("../assets/imagSkills", true);
 
-<<<<<<< HEAD
-function SkillList({name}) {
-
+function SkillList({ name }) {
   return (
     <div>
       <div className="card mb-2">
         <div className="card-body text-center bg-secondary p-0">
           <div className="col d-flex justify-content-center ">
             <div className="row d-inline-flex">
-                <img src={imageSkill(`./${name}.png`)} className=""alt="" />
+              <img src={imageSkill(`./${name}.png`)} className="" alt="" />
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default SkillList;
