85e8cf20c41ba75a4a5f7e6494e3bddde54af858
drios8331 <73044961+drios8331@users.noreply.github.com>
2022-11-02 23:54:24 -0500

    Cards 50%

--- src/App.jsx
+++ src/App.jsx
@@ -7,54 +7,69 @@ import CardOthers from "./components/Card_Others";
 // import Coffe from "./components/Coffe";
 // import BotonFloat from "./components/Boton_flotante";
 import ButtonCoffe from "./components/Button_coffe";
+// import { Icon123 } from "react-bootstrap-icons";
 // import './script';
 // import skills from "./skills";
 
 export function App() {
-  return (
-    <>
-      <div className="container-xxl" style={{ height: "100%" }}>
-        <div className="container-fluid">
-          <div className="row" style={{ paddingTop: "10%" }}>
-            <div className="col-xl-10 col-lg-10 col-md-8 col-sm-8 col-8 mt-5 position-relative">
-              <CardMaster
-                imagen="Foto_David"
-                p="Experiencia de 6 años en de cargos administrativos enfocados al manejo de sistemas informáticos, visionario y proactivo, con conocimientos avanzados en Sistemas Windows, Office, Internet, desarrollo de software (Laravel PhP, React JS, HTML, CSS, JavaScript, Bootstrap, Composer, npm, JQuery, Ajax), diseño y administración de BD(MySQL), manejo de Git / Github. Con capacidad de trabajo y actitud proactiva en el planteamiento, análisis y solución de problemas. Adecuadas relaciones interpersonales y habilidades para el trabajo en equipo."
-                title="David Rios Mejia"
-              />
-              <hr />
-              <CardExp title="Experiencia laboral" />
-              <hr />
-              <CardStudy title="Educación" />
-              <hr />
-              <CardProyects
-                proyectName="ProyectoSena"
-                image1="FotoSlaider3"
-                image2="FotoSlaider2"
-                image3="FotoSlaider1"
-                title="LYD System"
-                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur assumenda adipisci dolorem quas odio excepturi saepe officiis hic et, sint nesciunt nam, reiciendis dicta, omnis dolor ipsam numquam commodi eos!"
-              />
-              <hr />
-              {/* <CardProyects /> */}
-              <hr />
-              {/* <CardProyects /> */}
-              <hr />
-              {/* <CardProyects /> */}
-              <hr />
-              {/* <CardProyects /> */}
-              <hr />
-              {/* <CardProyects /> */}
-              <hr />
-              <CardOthers />
-              <ButtonCoffe className="position-absolute" />
+    return (
+        <>
+            <div className="container-xxl" style={{ height: "100%" }}>
+                <div className="container-fluid">
+                    <div className="row" style={{ paddingTop: "10%" }}>
+                        <div className="col-xl-10 col-lg-10 col-md-8 col-sm-8 col-8 mt-5 position-relative">
+                            <CardMaster
+                                imagen="Foto_David"
+                                p="Experiencia de 6 años en de cargos administrativos enfocados al manejo de sistemas informáticos, visionario y proactivo, con conocimientos avanzados en Sistemas Windows, Office, Internet, desarrollo de software (Laravel PhP, React JS, HTML, CSS, JavaScript, Bootstrap, Composer, npm, JQuery, Ajax), diseño y administración de BD(MySQL), manejo de Git / Github. Con capacidad de trabajo y actitud proactiva en el planteamiento, análisis y solución de problemas. Adecuadas relaciones interpersonales y habilidades para el trabajo en equipo."
+                                title="David Rios Mejia"
+                            />
+                            <hr />
+                            <CardExp title="Experiencia laboral" />
+                            <hr />
+                            <CardStudy title="Educación" />
+                            <hr />
+                            <CardProyects
+                                proyectName="ProyectoSena"
+                                image1="FotoSlaider3"
+                                image2="FotoSlaider2"
+                                image3="FotoSlaider1"
+                                title="LYD System"
+                                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur assumenda adipisci dolorem quas odio excepturi saepe officiis hic et, sint nesciunt nam, reiciendis dicta, omnis dolor ipsam numquam commodi eos!"
+                            />
+                            <hr />
+                            <CardProyects
+                                proyectName="ReservasVehiculos"
+                                image1="Reservasok"
+                                image2="Reservasok1"
+                                image3="Reservasok3"
+                                title="Reservas Vehiculares"
+                                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur assumenda adipisci dolorem quas odio excepturi saepe officiis hic et, sint nesciunt nam, reiciendis dicta, omnis dolor ipsam numquam commodi eos!"
+                            />
+                            <hr />
+                            <CardProyects
+                                proyectName="ControlEscoltas"
+                                image1="Escoltas1"
+                                image2="Escoltas2"
+                                image3="Escoltas3"
+                                title="Control de Escoltas"
+                                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur assumenda adipisci dolorem quas odio excepturi saepe officiis hic et, sint nesciunt nam, reiciendis dicta, omnis dolor ipsam numquam commodi eos!"
+                            />
+                            <hr />
+                            {/* <CardProyects /> */}
+                            <hr />
+                            {/* <CardProyects /> */}
+                            <hr />
+                            {/* <CardProyects /> */}
+                            <hr />
+                            <CardOthers />
+                            <ButtonCoffe className="position-absolute" />
+                        </div>
+                        <div className="col-xl-2 col-lg-2 col-md-4 col-sm-4 col-4 mt-5">
+                            <Sidebar className="position-relative" />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="col-xl-2 col-lg-2 col-md-4 col-sm-4 col-4 mt-5">
-              <Sidebar className="position-relative" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/components/Card_Others.jsx
+++ src/components/Card_Others.jsx
@@ -1,41 +1,47 @@
 import Card from "./Card";
+import Image from "./Image";
 
 function Card_Others() {
-  return (
-    <>
-      <Card>
-        <div className="fs-4 text-center fw-bold mb-3"> Otros Conocimientos</div>
-        <div className="row mb-3">
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-            <div className="col-3 profyle">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure odit suscipit maiores repellendus vitae nobis, vero quo nostrum animi odio, voluptatem pariatur excepturi voluptatibus voluptate eaque id ad neque?
-            </div>
-        </div>
-      </Card>
-    </>
-  );
+    return (
+        <>
+            <Card>
+                <div className="fs-4 text-center fw-bold mb-3">
+                    Otros Conocimientos
+                </div>
+                <div className="row mb-3">
+                    <div className="col-3 text-center pt-2">
+                        <Image nameImage="dtr720" classImage="w-75" />
+                    </div>
+                    <div className="col-3 profyle">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dignissimos iure odit suscipit maiores repellendus vitae
+                        nobis, vero quo nostrum animi odio, voluptatem pariatur
+                        excepturi voluptatibus voluptate eaque id ad neque?
+                    </div>
+                    <div className="col-3 profyle text-center pt-4">
+                        <Image nameImage="Computador" classImage="w-75" />
+                    </div>
+                    <div className="col-3 profyle">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dignissimos iure odit suscipit maiores repellendus vitae
+                        nobis, vero quo nostrum animi odio, voluptatem pariatur
+                        excepturi voluptatibus voluptate eaque id ad neque?
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-3 text-center pt-5">
+                        <Image nameImage="Celular" classImage="w-50" />
+                    </div>
+                    <div className="col-3 profyle">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dignissimos iure odit suscipit maiores repellendus vitae
+                        nobis, vero quo nostrum animi odio, voluptatem pariatur
+                        excepturi voluptatibus voluptate eaque id ad neque?
+                    </div>
+                </div>
+            </Card>
+        </>
+    );
 }
 
 export default Card_Others;
